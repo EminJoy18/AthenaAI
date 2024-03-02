f315c9ed7926bdb8c67303b2cae413d55ab6b211
Emin Joy <97777160+EminJoy18@users.noreply.github.com>
2024-03-02 17:14:27 +0530

    check

--- imp.py
+++
@@ -1,9 +0,0 @@
-import pyttsx3
-
-def text_to_speech(text):
-    engine = pyttsx3.init()
-    engine.say(text)
-    engine.runAndWait()
-
-text = "Hello, I am Emin Joy. How are you?"
-text_to_speech(text)
